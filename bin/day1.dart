1dba7467bf0736412ad4493f0722f47704997af7
soekhant01 <soekhant.dev@gmail.com>
2026-04-09 21:49:33 +0630

    day1 (type inference var,final,dynamic types)

--- bin/day1.dart
+++ bin/day1.dart
@@ -1,4 +1,4 @@
-void main(){
+void main() {
   String name = "Soe Khant";
   int age = 28;
 
@@ -7,4 +7,25 @@ void main(){
   double? height = 5.6;
 
   print("$name is $age years old, and he is $height tall");
+
+  // type inference
+
+  // var can store any type in first time, but if override, type of value can't change
+  var stringData = "soe khant";
+  var intData = 12;
+  stringData = "ko gyi soe";
+
+  // final is also type inference but it cannot override assign value
+  final finalString = "final";
+  final finalDouble = 12.6;
+
+  //   dynamic, if override, type of value can change
+  dynamic dynamicData = 12;
+  dynamicData = "string";
+
+  //   sub type, type of child can sign in parent, Object is parent of all type in dart
+  int a = 5;
+  num parentOfIntType = 5;
+  num parentOfDoubleType = 10.9;
+  Object parentOfAllType = "string";
 }
