5e02afb8a2dea5929e8b371aaf43f76b3f07b37b
soekhant01 <soekhant.dev@gmail.com>
2026-04-09 15:43:28 +0630

    day1 (null data type)

--- bin/day1.dart
+++ bin/day1.dart
@@ -2,5 +2,9 @@ void main(){
   String name = "Soe Khant";
   int age = 28;
 
-  print("$name is $age years old");
+  // nullable
+  String? userName;
+  double? height = 5.6;
+
+  print("$name is $age years old, and he is $height tall");
 }
