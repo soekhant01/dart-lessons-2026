ed9ee119d18b876c2028a0ccc3568d85b3dde81a
soekhant01 <soekhant.dev@gmail.com>
2026-04-10 09:35:48 +0630

    day2 (assignment_operator)

---
+++ bin/assignment_operator.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 5;
+  int b = 6;
+  num c = 25;
+
+  //  assignment operator ( +=, -=, *=, /=, ~/=, %= )
+  b += a;
+  b -= a;
+  b *= a;
+  c /= a;
+  b ~/= a;
+  b %= a;
+
+  print(c);
+  print(b);
+}
