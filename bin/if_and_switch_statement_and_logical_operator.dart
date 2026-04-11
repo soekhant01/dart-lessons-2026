31bd8e6188b3e753ab38b3f01c02cee7f6a4a75b
soekhant01 <soekhant.dev@gmail.com>
2026-04-11 17:19:24 +0630

    day3 (if statement and logical operator)

---
+++ bin/if_and_switch_statement_and_logical_operator.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 7;
+  if (a > 0 && a == 7) {
+    print("$a is luck seven");
+  } else if (a == 0) {
+    print("$a is zero");
+  } else {
+    print("$a is negative");
+  }
+}
