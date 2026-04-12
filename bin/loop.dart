34bd3744a001d9cdcfccd6d082dafedd2c0d96ad
soekhant01 <soekhant.dev@gmail.com>
2026-04-12 12:11:13 +0630

    day-4 (for in loop, forEach)

--- bin/loop.dart
+++ bin/loop.dart
@@ -18,4 +18,23 @@ void main() {
     print(i);
     i++;
   } while (i < 10);
+
+  print(
+    "========= for ... in loop ===========",
+  ); // often use for list, set, map
+  List<int> numList = [1, 3, 2, 5, 6, 7, 8];
+  for (int i in numList) {
+    // if you want to print like order, you should use this loop
+    print(i);
+  }
+
+  Map<String, String> person = {'name': 'Mg Mg', 'age': '22'};
+  for (MapEntry<String, String> p in person.entries) {
+    print('${p.key} is ${p.value}');
+  }
+
+  print("========= forEach ===========");
+  numList.forEach((int n) => print(n));
+  
+  person.forEach((key, value) => print('$key is $value'));
 }
