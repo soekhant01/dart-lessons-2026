575e13be176f9450deabd199e8dd86ca1669e3e0
soekhant01 <soekhant.dev@gmail.com>
2026-06-26 06:38:02 +0630

    use (on) keyword for exception and rethrow

--- bin/exception_handling.dart
+++ bin/exception_handling.dart
@@ -7,9 +7,31 @@ void main() {
     print("end of try");
   } catch (e) {
     print("The error is $e");
+    print(e.runtimeType);
   } finally {
     // finally can work either error or success
     print("finally");
   }
-  print("end of program");
+  print(
+    "=======================================================================",
+  );
+
+  List a = [];
+  String? s;
+  try {
+    print(a[1]);
+    print(int.parse(a[0]));
+    print(s!.length);
+  } on RangeError catch (e) {
+    print("please careful for index, $e occur");
+    if (a.isEmpty) {
+      rethrow;
+    }
+  } on FormatException {
+    print("string can't cast to number");
+  } catch (e) {
+    print(e.runtimeType);
+  } finally {
+    print("Finally");
+  }
 }
